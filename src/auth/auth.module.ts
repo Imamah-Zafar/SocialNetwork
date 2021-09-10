f274ef5b764d7faa0b0fb3248878af371235f734
Imamah-Zafar <imamah.zafar@tintash.com>
2021-09-10 14:55:25 +0500

    Adding Authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/users/user.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { jwtConstants } from './constant';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  providers: [AuthService, LocalStrategy,JwtStrategy],
+  imports: [
+    forwardRef(() =>UserModule),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1000s' },
+    }),],
+  exports: [AuthService]
+})
+export class AuthModule { }
