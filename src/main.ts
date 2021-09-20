bcfe71b83c23574449b01b623939b17cfa1e4aea
Imamah-Zafar <imamah.zafar@tintash.com>
2021-09-20 16:37:41 +0500

    third commit

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,16 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 import sslRedirect from 'heroku-ssl-redirect';
 
+
 async function bootstrap() {
   const PORT :string= process.env.PORT 
 
-
+  const favicon = require('express-favicon');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'static'));
   app.use(sslRedirect())
+  app.use(favicon(__dirname + '/public/favicon.png'));
   const config = new DocumentBuilder()
     .setTitle('Social Media APP')
     .setVersion('1.0')
